37a7af276906ed3fae800d5eabb223e96a5908c8
bertreux <115898406+bertreux@users.noreply.github.com>
2024-03-27 18:16:06 +0100

    cours scrapy next page

--- scrapy/testScrapy/testScrapy/spiders/quotes_spider.py
+++ scrapy/testScrapy/testScrapy/spiders/quotes_spider.py
@@ -14,3 +14,8 @@ class QuoteSpider(scrapy.Spider):
              'about' : 'http://quotes.toscrape.com'+q.css('span a').attrib['href'],
              'tags' : {tmp: None for tmp in q.css('div.tags a.tag::text').getall()}
             }
+
+        next_page = response.css('ul.pager li.next a').attrib['href']
+        if next_page is not None:
+            next_page_url = 'http://quotes.toscrape.com' + next_page
+            yield response.follow(next_page_url, callback=self.parse)
